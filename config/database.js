94ca4d3db7b8191b20020ef4fb56d3ff596bf3e7
Frédéric Lang <f.lang@hotmail.fr>
2020-07-01 14:11:37 +0200

    new database config

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+//        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
         useNullAsDefault: true,
